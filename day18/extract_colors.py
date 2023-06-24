168d0965c444c2ff83d88b42326c4f2042965ea3
Bertina S <76837815+bertinaa@users.noreply.github.com>
2023-06-24 14:48:23 +0530

    Create extract_colors.py

---
+++ day18/extract_colors.py
@@ -0,0 +1,18 @@
+import colorgram
+
+""" 
+just to extract the colors we did this
+colors = colorgram.extract('dots.jpg',30)
+# colorgram.extract returns Color objects, which let you access
+# RGB, HSL, and what proportion of the image was that color.
+#HSL(Hue-Saturation-Lightness)
+color_palette = []
+for item in colors:
+    r = item.rgb.r
+    g = item.rgb.g
+    b = item.rgb.b
+    color = (r,g,b)
+    color_palette.append(color)
+print(color_palette)
+"""
+colors = [ (202, 166, 109), (152, 73, 47), (170, 153, 41), (222, 202, 138), (53, 93, 124), (135, 32, 22), (132, 163, 184), (48, 118, 88), (198, 91, 71), (16, 97, 75), (100, 73, 75), (67, 47, 41), (147, 178, 147), (163, 142, 156), (234, 177, 165), (55, 46, 50), (130, 28, 31), (184, 205, 174), (41, 60, 72), (83, 147, 126), (181, 87, 90), (31, 77, 84), (47, 65, 83), (215, 177, 182), (19, 71, 63), (175, 192, 212)]
