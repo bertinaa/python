84706cd5fd4735677ada29035fb4f61cc981168d
Bertina S <76837815+bertinaa@users.noreply.github.com>
2023-07-12 13:41:51 +0530

    Create main.py

---
+++ day20/main.py
@@ -0,0 +1,29 @@
+MOVE_DISTANCE = 20
+from turtle import Screen, Turtle
+import time
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
+game_on = True
+while game_on:
+    snake.move()
+    screen.update()
+    time.sleep(0.1)
+
+
+
+
+screen.exitonclick()
