64cb8c8c91544ffff7ea74efd80fe3abd68e1f77
Bertina S <76837815+bertinaa@users.noreply.github.com>
2023-07-18 12:17:13 +0530

    Update main.py

--- day24/Automating_names/main.py
+++ day24/Automating_names/main.py
@@ -8,12 +8,13 @@ PLACEHOLDER = "[name]"
 
 with open ("Input/Names/invited_names.txt") as names:
     invited_names = names.readlines() #invited_names is a list of all the names now
+    #names by default will have \n
 
 with open("Input/Letters/starting_letter.txt", "r") as starting_letter:
     letter_content = starting_letter.read()
 
 for name in invited_names:
-    stripped_name = name.strip()
+    stripped_name = name.strip() #to remove \n
     #note : this doesn't work if you do name.strip()
     with open(f"Output/ReadyToSend/{stripped_name}.txt","w") as new_letter:
         final_text = letter_content.replace(PLACEHOLDER,stripped_name)
