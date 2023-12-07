163a74038177bf798a366f282de506bac69aa5e5
Bertina S <76837815+bertinaa@users.noreply.github.com>
2023-12-07 10:29:25 +0530

    Create bmi.py

---
+++ day2/bmi.py
@@ -0,0 +1,7 @@
+height = input()
+weight = input()
+# Your code below this line 👇
+weight_as_int = int(weight)
+height_as_float = float(height)
+# Using the exponent operator **
+bmi = weight_as_int / height_as_float ** 2
