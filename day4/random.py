d8808818aedc1add9506b10114634eca112568f6
Bertina S <76837815+bertinaa@users.noreply.github.com>
2023-12-08 19:31:39 +0530

    Create random.py

---
+++ day4/random.py
@@ -0,0 +1,8 @@
+names = names_string.split(", ")
+# The code above converts the input into an array seperating
+#each name in the input by a comma and space.
+# 🚨 Don't change the code above 👆
+
+import random
+person = random.choice(names)
+print(f"{person} is going to buy the meal today!")
