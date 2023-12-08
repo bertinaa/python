06ff780305272cf4638754c729ec7305a8db3a61
Bertina S <76837815+bertinaa@users.noreply.github.com>
2023-12-08 20:27:41 +0530

    Update treasureMap.py

--- day4/treasureMap.py
+++ day4/treasureMap.py
@@ -13,3 +13,35 @@ number_index = int(position[1]) - 1
 map[number_index][letter_index] = "X"
 
 print(f"{line1}\n{line2}\n{line3}")
+
+#mycode
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input() # Where do you want to put the treasure?
+# 🚨 Don't change the code above 👆
+# Write your code below this row 👇
+ # a1,a2,a3 = 0
+ # b1,b2,b3 = 1
+ # c1,c2,c3 = 2
+
+if position[0]=='A':
+  b = 0
+elif position[0]=='B':
+  b = 1
+else:
+  b = 2
+if position[1] == '1':
+  a = 0
+elif position[1] == '2':
+  a = 1
+else:
+  a = 2
+
+map[a][b] = 'X'
+
+# Write your code above this row 👆
+# 🚨 Don't change the code below 👇
+print(f"{line1}\n{line2}\n{line3}")
